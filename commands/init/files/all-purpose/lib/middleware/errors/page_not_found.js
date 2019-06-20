bc3e9694d7c705bb8b06ae8d9d16edc97e18e421
Andy Bettisworth <andy@accreu.com>
2019-06-20 02:29:42 -0500

    Improve require_body middleware

--- commands/init/files/all-purpose/lib/middleware/errors/page_not_found.js
+++ commands/init/files/all-purpose/lib/middleware/errors/page_not_found.js
@@ -7,7 +7,7 @@
 module.exports = (req, res, next) => {
   let err = new Error()
   err.status  = 404
-  err.message = "Not found"
+  err.message = 'Not found'
 
   next(err)
 }
--- commands/init/files/express-turbolinks/lib/middleware/errors/require_body.js
+++ commands/init/files/express-turbolinks/lib/middleware/errors/require_body.js
@@ -7,17 +7,18 @@
 function require_body(keys) {
   return (req, res, next) => {
     if (!req.body || Object.keys(req.body).length === 0) {
-      return res.status(400).json({ error: { message: 'Missing request body.' } })
+      return res.status(400).json({ error: { message: 'Missing request body' } })
     }
 
-    let body_keys = Object.keys(req.body)
-    let missing_keys = keys.filter(x => !body_keys.includes(x))
+    let missing_keys = keys.filter(key => !Object.keys(req.body).includes(key))
 
-    if (missing_keys.length === 0) {
-      next()
-    } else {
-      res.status(400).json({ error: { message: `Missing fields: ${missing_keys.join(', ')}.` } })
+    if (missing_keys.length > 0) {
+      return res.status(422).json({ error: {
+        message: `Missing fields: ${missing_keys.join(' ')}`
+      }})
     }
+
+    next()
   }
 }
 
