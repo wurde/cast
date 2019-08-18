cad946506c83d728d391d2fb1a9428049ffd808d
Andy Bettisworth <andy@accreu.com>
2019-08-18 02:12:12 -0500

    Handle clockin and clockout

--- commands/work.js
+++ commands/work.js
@@ -1,5 +1,11 @@
 'use strict'
 
 module.exports = (argv) => {
-  console.log('Work')
+  if (argv[3] === 'clockin') {
+    console.log(`Work CLOCKIN`)
+  } else if (argv[3] === 'clockout') {
+    console.log(`Work CLOCKOUT`)
+  } else {
+    console.log(`Work REPORT`)
+  }
 }
