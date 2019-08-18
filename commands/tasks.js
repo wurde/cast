c9923718fd8246bfb95c27a391fc10f339473957
Andy Bettisworth <andy@accreu.com>
2019-08-18 17:25:33 -0500

    Add CLI parser

--- commands/tasks.js
+++ commands/tasks.js
@@ -1,5 +1,52 @@
 'use strict'
 
-module.exports = (argv) => {
+/**
+ * Dependencies
+ */
+
+const meow = require('meow')
+
+/**
+ * Parse args
+ */
+
+const cli = meow(`
+  Usage
+    $ cast tasks <input>
+
+  Options
+    --create, -c  Create a task.
+    --done, -d  Mark task as done.
+    --remove  Remove a specific task.
+    --clear  Clear all tasks marked as done.
+`, {
+  flags: {
+    create: {
+      type: 'boolean',
+      alias: 'c'
+    },
+    done: {
+      type: 'boolean',
+      alias: 'd'
+    },
+    clear: {
+      type: 'boolean'
+    }
+  }
+})
+
+/**
+ * Define script
+ */
+
+function tasks(argv) {
   console.log('Tasks')
+  console.log('input', cli.input[1])
+  console.log('flags', cli.flags)
 }
+
+/**
+ * Export script
+ */
+
+module.exports = tasks
