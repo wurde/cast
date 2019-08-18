a88d6e69b9bd96339f0b3e78b17a4d7bb18d6acd
Andy Bettisworth <andy@accreu.com>
2019-08-18 02:34:07 -0500

    Setup db client and install sqlite3

--- commands/work.js
+++ commands/work.js
@@ -1,11 +1,52 @@
 'use strict'
 
-module.exports = (argv) => {
-  if (argv[3] === 'clockin') {
-    console.log(`Work CLOCKIN`)
-  } else if (argv[3] === 'clockout') {
-    console.log(`Work CLOCKOUT`)
-  } else {
-    console.log(`Work REPORT`)
+/**
+ * Dependencies
+ */
+
+const path = require('path')
+const fs = require('fs')
+const knex = require('knex')
+
+/**
+ * Constants
+ */
+
+const work_db = path.join(process.env.HOME, '.work.sqlite3')
+const db = knex({
+  client: 'sqlite3',
+  useNullAsDefault: false,
+  connection: { filename: work_db }
+})
+
+/**
+ * Setup database
+ */
+
+function setup_database() {
+  if (fs.existsSync(work_db) === false) {
+    console.log('setup_database')
   }
 }
+
+/**
+ * Define script
+ */
+
+function work(argv) {
+  setup_database()
+
+  // if (argv[3] === 'clockin') {
+  //   console.log(`Work CLOCKIN`)
+  // } else if (argv[3] === 'clockout') {
+  //   console.log(`Work CLOCKOUT`)
+  // } else {
+  //   console.log(`Work REPORT`)
+  // }
+}
+
+/**
+ * Export script
+ */
+
+module.exports = work
