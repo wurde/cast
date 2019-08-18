3daf6bcfb68c3a3f0b2d191fde1c64a5337e8205
Andy Bettisworth <andy@accreu.com>
2019-08-18 05:05:59 -0500

    Setup database with Sequelize

--- commands/work.js
+++ commands/work.js
@@ -6,43 +6,55 @@
 
 const path = require('path')
 const fs = require('fs')
-const knex = require('knex')
+const Sequelize = require('sequelize')
 
 /**
  * Constants
  */
 
 const work_db = path.join(process.env.HOME, '.work.sqlite3')
-const db = knex({
-  client: 'sqlite3',
-  useNullAsDefault: false,
-  connection: { filename: work_db }
-})
+const db = new Sequelize({ dialect: 'sqlite', storage: work_db, logging: false })
 
 /**
  * Setup database
  */
 
-function setup_database() {
-  if (fs.existsSync(work_db) === false) {
-    console.log('setup_database')
+async function setup_database() {
+  try {
+    const [results, _] = await db.query("SELECT name FROM sqlite_master WHERE type='table' AND name='entries';")
+
+    if (results.length === 0) {
+      await db.query('CREATE TABLE IF NOT EXISTS entries (id integer primary key, working_directory text, start_at text, end_at text);')
+    }
+  } catch(err) {
+    console.error(err)
   }
+}
+
+async function close_database() {
+  await db.close()
 }
 
 /**
  * Define script
  */
 
-function work(argv) {
-  setup_database()
+async function work(argv) {
+  try {
+    await setup_database()
 
-  // if (argv[3] === 'clockin') {
-  //   console.log(`Work CLOCKIN`)
-  // } else if (argv[3] === 'clockout') {
-  //   console.log(`Work CLOCKOUT`)
-  // } else {
-  //   console.log(`Work REPORT`)
-  // }
+    if (argv[3] === 'clockin') {
+      console.log(`Work CLOCKIN`)
+    } else if (argv[3] === 'clockout') {
+      console.log(`Work CLOCKOUT`)
+    } else {
+      console.log(`Work REPORT`)
+    }
+
+    await close_database()
+  } catch(err) {
+    console.error(err)
+  }
 }
 
 /**
