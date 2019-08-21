52d2437363b01f5c719bbfe75a30aff401188e76
Andy Bettisworth <andy@accreu.com>
2019-08-21 10:49:56 -0500

    Get rid of infinite loop via event listeners

--- commands/pdf.js
+++ commands/pdf.js
@@ -8,6 +8,7 @@ const fs = require('fs')
 const path = require('path')
 const meow = require('meow')
 const marked = require('marked')
+const nodemon = require('nodemon')
 const puppeteer = require('puppeteer')
 
 /**
@@ -20,7 +21,7 @@ const cli = meow(`
 
   Options
     --watch, -w   Automatically re-render when changes are detected (Default: false).
-    --stylesheet  Path to a local style sheet with CSS.
+    --stylesheet  Path to a local CSS style sheet.
 `, {
   flags: {
     watch: {
@@ -34,6 +35,26 @@ const cli = meow(`
 })
 
 /**
+ * Define helper
+ */
+
+async function render_page(html) {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.setContent(html)
+  return [browser, page]
+}
+
+async function style_page(page) {
+  if (cli.flags.stylesheet) {
+    page.addStyleTag({ content: fs.readFileSync(cli.flags.stylesheet) })
+  } else {
+    // TODO apply default styles
+  }
+  return page
+}
+
+/**
  * Define script
  */
 
@@ -41,22 +62,37 @@ async function pdf(argv) {
   if (cli.flags.h) cli.showHelp()
   if (cli.input.length < 2) cli.showHelp()
   if (!fs.existsSync(cli.input[1])) cli.showHelp()
+  if (cli.flags.stylesheet && !fs.existsSync(cli.flags.stylesheet)) cli.showHelp()
 
   const basename = path.basename(cli.input[1], path.extname(cli.input[1]))
 
-  // TODO if cli.flags.stylesheet then apply custom CSS.
-  // TODO if cli.flags.watch then use nodemon to listen for changes.
-
   const html = marked(fs.readFileSync(cli.input[1], 'utf8'))
   fs.writeFileSync('temp.html', html)
+  
+  if (cli.flags.watch || cli.flags.w) {
+    nodemon(`--watch temp.html --exec echo Updating PDF...`)
 
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  await page.setContent(html)
-  await page.pdf({ path: `${basename}.pdf` })
-  browser.close()
-
-  fs.unlinkSync('temp.html')
+    nodemon.on('start', async () => {
+      console.log('App has started')
+      let [browser, page] = await render_page(html)
+      page = await style_page(html)
+      await page.pdf({ path: `${basename}.pdf` })
+      }).on('quit', () => {
+      console.log('App has quit')
+      browser.close()
+      fs.unlinkSync('temp.html')
+    }).on('restart', (files) => {
+      process.exit()
+      console.log('App restarted due to: ', files)
+    })
+  } else {
+    let [browser, page] = await render_page(html)
+    page = await style_page(html)
+    await page.pdf({ path: `${basename}.pdf` })
+    
+    browser.close()
+    fs.unlinkSync('temp.html')
+  }
 }
 
 /**
