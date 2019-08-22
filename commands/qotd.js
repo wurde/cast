be3ee83ead6a0fc451a21ab1e9c172b646a73a92
Andy Bettisworth <andy@accreu.com>
2019-08-22 06:06:25 -0500

    Use randomInteger for grabbing random Author

--- commands/qotd.js
+++ commands/qotd.js
@@ -5,6 +5,7 @@
  */
 
 const https = require('https')
+const querystring = require('querystring')
 const meow = require('meow')
 const figlet = require('figlet')
 
@@ -15,8 +16,8 @@ const figlet = require('figlet')
 const API_URL = 'https://en.wikiquote.org/w/api.php'
 const AUTHORS = [
   'Albert Einstein',
+  'Steve Jobs',
   'Walt Disney',
-  'Steve Jobs'
 ]
 
 /**
@@ -29,17 +30,41 @@ const cli = meow(`
 `)
 
 /**
+ * Define helper
+ */
+
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+/**
  * Define script
  */
 
 async function qotd() {
   if (cli.flags.h) cli.showHelp()
 
-  https.get(API_URL, res => {
-    console.log('statusCode:', res.statusCode)
+  https.get(`${API_URL}?${querystring.stringify({
+    format: 'json',
+    action: 'query',
+    redirects: '',
+    titles: AUTHORS[randomInteger(0, AUTHORS.length - 1)]
+  })}`, res => {
+    let data = ''
 
-    figlet.text('Quote of the day', (err, data) => {
-      console.log(data)
+    res.on('data', (chunk) => {
+      data += chunk
+    })
+
+    res.on('end', () => {
+      data = JSON.parse(data)
+      const pages = data.query.pages
+
+      console.log('end', pages)
+
+      figlet.text('Quote of the day', (err, data) => {
+        console.log(data)
+      })
     })
   })
 }
