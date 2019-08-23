dae8d4431e5cc95300775b538de9d31a5067345c
Andy Bettisworth <andy@accreu.com>
2019-08-23 01:35:12 -0500

    Promisfy the https request

--- commands/qotd.js
+++ commands/qotd.js
@@ -33,11 +33,29 @@ const cli = meow(`
 `)
 
 /**
- * Define helper
+ * Define helpers
  */
 
 function randomInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function quote_api_request(obj) {
+  return new Promise((resolve, reject) => {
+    const q = querystring.stringify(obj)
+
+    const request = https.get(`${API_URL}?${q}`, res => {
+      if (res.statusCode < 200 || res.statusCode > 299) {
+        reject(new Error('Page load error: ' + res.statusCode))
+      }
+
+      let data = ''
+      res.on('data', chunk => data += chunk)
+      res.on('end', () => resolve(JSON.parse(data)))
+    })
+
+    request.on('error', err => reject(err))
+  })
 }
 
 /**
@@ -53,93 +71,109 @@ async function qotd() {
    * Get the page ID for a given author.
    */
 
-  https.get(`${API_URL}?${querystring.stringify({
+  let page_data = await quote_api_request({
     format: 'json',
     action: 'query',
     redirects: '',
     titles: author
-  })}`, res => {
-    let data = ''
-
-    res.on('data', (chunk) => {
-      data += chunk
-    })
-
-    res.on('end', () => {
-      data = JSON.parse(data)
-
-      let pageIDs = Object.keys(data.query.pages)
-      pageIDs = pageIDs.filter(id => Number(id) > 0)
-
-      if (pageIDs.length > 0) {
-        /**
-         * Get sections for a given page.
-         */
-
-        https.get(`${API_URL}?${querystring.stringify({
-          format: 'json',
-          action: 'parse',
-          prop: 'sections',
-          pageid: pageIDs[0]
-        })}`, res => {
-          let data = ''
-
-          res.on('data', (chunk) => {
-            data += chunk
-          })
-
-          res.on('end', () => {
-            data = JSON.parse(data)
-
-            let sections = data.parse.sections
-
-            // TODO random section
-            const sectionID = randomInteger(1,4)
-
-            /**
-             * Get quotes for a given section.
-             */
-
-            https.get(`${API_URL}?${querystring.stringify({
-              format: 'json',
-              action: 'parse',
-              noimages: '',
-              pageid: pageIDs[0],
-              section: sectionID
-            })}`, res => {
-              let data = ''
-
-              res.on('data', (chunk) => {
-                data += chunk
-              })
-
-              res.on('end', () => {
-                data = JSON.parse(data)
-
-                const text = data.parse.text["*"]
-                const $ = cheerio.load(text)
-                let quote = $('b').html()
-
-                quote = html_to_text.fromString(quote, { wordwrap: 300 })
-                quote = quote.replace(/\[.*?\]/g, '')
-                quote = quote.replace(/\s+/, ' ')
-
-                figlet.text("Quote of the day", {
-                  font: 'Small'
-                }, (err, data) => {
-                  console.log(data, '\n')
-                  console.log(chalk.bold(quote))
-                  console.log(chalk.bold(`- ${author}`))
-                })
-              })
-            })
-          })
-        })
-      } else {
-        console.error('No quotes found')
-      }
-    })
   })
+
+  console.log('page_data', page_data)
+  // let pageIDs = Object.keys(page_data.query.pages)
+  // pageIDs = pageIDs.filter(id => Number(id) > 0)
+  //
+  // if (pageIDs.length > 0) {
+  //   console.log('pageIDs', pageIDs)
+  // }
+
+  // TEMP
+  // https.get(`${API_URL}?${querystring.stringify({
+  //   format: 'json',
+  //   action: 'query',
+  //   redirects: '',
+  //   titles: author
+  // })}`, res => {
+  //   let data = ''
+  //
+  //   res.on('data', (chunk) => {
+  //     data += chunk
+  //   })
+  //
+  //   res.on('end', () => {
+  //     data = JSON.parse(data)
+  //
+  //     let pageIDs = Object.keys(data.query.pages)
+  //     pageIDs = pageIDs.filter(id => Number(id) > 0)
+  //
+  //     if (pageIDs.length > 0) {
+  //       /**
+  //        * Get sections for a given page.
+  //        */
+  //
+  //       https.get(`${API_URL}?${querystring.stringify({
+  //         format: 'json',
+  //         action: 'parse',
+  //         prop: 'sections',
+  //         pageid: pageIDs[0]
+  //       })}`, res => {
+  //         let data = ''
+  //
+  //         res.on('data', (chunk) => {
+  //           data += chunk
+  //         })
+  //
+  //         res.on('end', () => {
+  //           data = JSON.parse(data)
+  //
+  //           let sections = data.parse.sections
+  //
+  //           // TODO random section
+  //           const sectionID = randomInteger(1,4)
+  //
+  //           /**
+  //            * Get quotes for a given section.
+  //            */
+  //
+  //           https.get(`${API_URL}?${querystring.stringify({
+  //             format: 'json',
+  //             action: 'parse',
+  //             noimages: '',
+  //             pageid: pageIDs[0],
+  //             section: sectionID
+  //           })}`, res => {
+  //             let data = ''
+  //
+  //             res.on('data', (chunk) => {
+  //               data += chunk
+  //             })
+  //
+  //             res.on('end', () => {
+  //               data = JSON.parse(data)
+  //
+  //               const text = data.parse.text["*"]
+  //               const $ = cheerio.load(text)
+  //               let quote = $('b').html()
+  //
+  //               quote = html_to_text.fromString(quote, { wordwrap: 300 })
+  //               quote = quote.replace(/\[.*?\]/g, '')
+  //               quote = quote.replace(/\s+/, ' ')
+  //
+  //               figlet.text("Quote of the day", {
+  //                 font: 'Small'
+  //               }, (err, data) => {
+  //                 console.log(data, '\n')
+  //                 console.log(chalk.bold(quote))
+  //                 console.log(chalk.bold(`- ${author}`))
+  //               })
+  //             })
+  //           })
+  //         })
+  //       })
+  //     } else {
+  //       console.error('No quotes found')
+  //     }
+  //   })
+  // })
 }
 
 /**
