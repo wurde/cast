4f01479b379bd515bc5dcd65fbcf0eabae5618af
Andy Bettisworth <andy@accreu.com>
2019-08-23 22:11:52 -0500

    Saving results to output file

--- commands/scrape.js
+++ commands/scrape.js
@@ -6,6 +6,8 @@
 
 const meow = require('meow')
 const puppeteer = require('puppeteer')
+const prompts = require('prompts')
+const fs = require('fs')
 
 /**
  * Parse args
@@ -13,31 +15,27 @@ const puppeteer = require('puppeteer')
 
 const cli = meow(`
   Usage
-    $ cast scrape [options] <selector> <url>
-
-  Options
-    --url, -u   Automatically re-render when changes are detected.
-    --selector, -s   Automatically re-render when changes are detected.
-`, {
-  flags: {
-    url: {
-      type: 'text',
-    },
-    selector: {
-      type: 'text',
-    },
-  }
-})
+    $ cast scrape URL
+`)
 
 /**
  * Launch Page
  */
 
 async function launchPage() {
-  const browser = await puppeteer.launch({ headless: false })
+  const browser = await puppeteer.launch({ 
+    defaultViewport: {
+      width: 1024,
+      height: 800
+    }
+  })
   const page = await browser.newPage()
   
   return [browser, page]
+}
+
+function storeScrapedResults(results) {
+  fs.writeFile()
 }
 
 /**
@@ -46,14 +44,48 @@ async function launchPage() {
 
 async function scrape() {
   if (cli.flags.h) cli.showHelp()
-  if (!cli.flags.url) cli.showHelp()
+  if (!cli.input[1]) cli.showHelp()
 
-  const [browser, page] = await launchPage()
-  await page.goto(cli.flags.url)
+  const chosenSelector = await prompts({
+    type: 'text',
+    name: 'selector',
+    message: 'Enter a CSS selector to scrape the page'
+  })
   
-  setTimeout(() => {
+  // Launch the page and visit the given url
+  const [browser, page] = await launchPage()
+  await page.goto(cli.input[1])
+
+  try {
+    const results = await page.evaluate(selector => {
+      return Array.from(document.querySelectorAll(selector))
+             .map(el => el.innerHTML)
+    }, chosenSelector.selector)
+
+    const saveFilePrompt = await prompts({
+      type: 'confirm',
+      name: 'saveFile',
+      initial: true,
+      message: 'Do you want to save the results to a file? (Y/n)'
+    })
+
+    if (saveFilePrompt.saveFile) {
+      const filenamePrompt = await prompts({
+        type: 'text',
+        name: 'filename',
+        message: 'Enter the filename you\'d like to save to',
+      })
+
+      fs.writeFileSync(filenamePrompt.filename, results)
+    }
+  
+    // Log the results to the console
+    console.log('results:', results)
+  } catch(err) {
+    console.error(err)
+  } finally {
     browser.close()
-  }, 5000)
+  }
 }
 
 /**
