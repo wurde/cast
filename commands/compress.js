b765aa4ee87b8a7b018199d4e74b6399ca5ed99c
Andy Bettisworth <andy@accreu.com>
2019-08-28 02:19:21 -0500

    Collect and verify file inputs

--- commands/compress.js
+++ commands/compress.js
@@ -4,7 +4,15 @@
  * Dependencies
  */
 
+const fs = require('fs')
+const zlib = require('zlib')
 const meow = require('meow')
+
+/**
+ * Constants
+ */
+
+const gzip = zlib.createGzip()
 
 /**
  * Parse args
@@ -31,12 +39,20 @@ const cli = meow(`
 
 function compress() {
   if (cli.flags.h) cli.showHelp()
-  if (!cli.input.length < 2) cli.showHelp()
+  if (cli.input.length < 2) cli.showHelp()
+
+  let files = cli.input.slice(1, cli.input.length)
+  files = files.filter(file => fs.existsSync(file))
+  if (files.length < 1) cli.showHelp()
 
   if (cli.flags.unzip) {
-    console.log('Uncompress', cli.input)
+    for (let i = 0; i < files.length; i++) {
+      console.log('Uncompress', files[i])
+    }
   } else {
-    console.log('Compress', cli.input)
+    for (let i = 0; i < files.length; i++) {
+      console.log('Compress', files[i])
+    }
   }
 }
 
