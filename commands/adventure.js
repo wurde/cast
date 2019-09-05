c806a8105d100869fefcf638ef41694737fbe5e1
Andy Bettisworth <andy@accreu.com>
2019-09-05 06:42:07 -0500

    print and prompt

--- commands/adventure.js
+++ commands/adventure.js
@@ -5,6 +5,9 @@
  */
 
 const meow = require('meow')
+const chalk = require('chalk')
+const figlet = require('figlet')
+const prompts = require('prompts')
 const showHelp = require('../helpers/showHelp')
 
 /**
@@ -75,26 +78,48 @@ class Activity {
  * Define script
  */
 
-function adventure() {
+async function adventure() {
   showHelp(cli)
 
+  console.log('')
+  console.log(figlet.textSync('ADVENTURE', { font: 'Bright' }))
+  console.log('')
+
   let main_player = new Player()
+  console.log(chalk.white.bold('*You wake up*\n'))
+  // print(`// ${main_player.current_room).toUpperCase()}\n}`)
+  // print(main_player.current_room.description, '\n')
+  
+  while (true) {
+    // TODO Handle n,s,e,w for movement.
+    // TODO Handle get,take,drop for items.
+    // TODO Handle i for listing player inventory.
 
-  let first_area = new Area({ name: 'Outside', description: 'Any direction will do.'})
+    let response = await prompts({
+      type: 'select',
+      name: 'action',
+      // TODO change color based on health green to red
+      message: `${chalk.green.bold('☺')}`,
+      choices: [
+        { title: 'Move north', value: 'north' },
+        { title: 'Move south', value: 'south' },
+        { title: 'Move east', value: 'east' },
+        { title: 'Move west', value: 'west' },
+        { title: 'Quit', value: 'quit' },
+      ]
+    })
+    console.log('')
 
-  let first_item = new Item({
-    name: 'Glowing Ignot',
-    description: 'You pickup something glowing on the ground.',
-    health: -15
-  })
+    let first_area = new Area({ name: 'Outside', description: 'Any direction will do.' })
+    let first_item = new Item({ name: 'Glowing Ignot', description: 'You pickup something glowing on the ground.', health: -15 })
+    let first_activity = new Activity({ name: 'Apple Tree', description: 'You reach up and eat an apple.', health: 5 })
 
-  let first_activity = new Activity({
-    name: 'Apple Tree',
-    description: 'You reach up and eat an apple.',
-    health: 5
-  })
-
-  console.log('Adventure', main_player, first_area, first_item,  first_activity)
+    if (response.action === 'q' || response.action === 'quit') {
+      console.log(chalk.red.bold('*Death by exhaustion*\n'))
+      console.log(chalk.white.bold('// GAME OVER\n'))
+      process.exit(0)
+    }
+  }
 }
 
 /**
