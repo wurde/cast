f06ab2367e922dd7cb2f0d948f03d74d3cca7bc2
Andy Bettisworth <andy@accreu.com>
2019-09-05 05:17:42 -0500

    define Activity and Item

--- commands/adventure.js
+++ commands/adventure.js
@@ -36,6 +36,42 @@ class Area {
 }
 
 /**
+ * Define Item
+ */
+
+class Item {
+  constructor({ name, description, health }) {
+    this.name = name
+    this.description = description
+    this.health = health
+
+    if (this.health > 0) {
+      this.rng = Math.floor(Math.random() * 100)
+    } else {
+      this.rng = Math.floor(Math.random() * 100) + 15
+    }
+  }
+}
+
+/**
+ * Define Activity
+ */
+
+class Activity {
+  constructor({ name, description, health }) {
+    this.name = name
+    this.description = description
+    this.health = health
+
+    if (this.health > 0) {
+      this.rng = Math.floor(Math.random() * 100)
+    } else {
+      this.rng = Math.floor(Math.random() * 100) + 15
+    }
+  }
+}
+
+/**
  * Define script
  */
 
@@ -46,7 +82,19 @@ function adventure() {
 
   let first_area = new Area({ name: 'Outside', description: 'Any direction will do.'})
 
-  console.log('Adventure', main_player, first_area)
+  let first_item = new Item({
+    name: 'Glowing Ignot',
+    description: 'You pickup something glowing on the ground.',
+    health: -15
+  })
+
+  let first_activity = new Activity({
+    name: 'Apple Tree',
+    description: 'You reach up and eat an apple.',
+    health: 5
+  })
+
+  console.log('Adventure', main_player, first_area, first_item,  first_activity)
 }
 
 /**
