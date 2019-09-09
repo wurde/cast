acf650a90309d4c6e445bd11fb0593c12dc759e8
Andy Bettisworth <andy@accreu.com>
2019-09-08 21:02:10 -0500

    Changed signature to handle user input.
    Co-authored-by: Moses Samuel <mosamuel98@gmail.com>

--- commands/delete-all.js
+++ commands/delete-all.js
@@ -6,6 +6,9 @@
 
 const meow = require('meow')
 const showHelp = require('../helpers/showHelp')
+const path = require('path')
+const chalk = require('chalk')
+const fs = require('fs')
 
 /**
  * Parse args
@@ -13,32 +16,50 @@ const showHelp = require('../helpers/showHelp')
 
 const cli = meow(`
   Usage
-    $ cast delete-all DEST_DIR MATCH
-`)
+    $ cast delete-all MATCH
+
+  Options:
+    --destination, -d DIR   Destination to start removal.
+`, {
+  flags: {
+    destDir: {
+      type: 'string',
+      alias: 'd'
+    }
+  }
+})
 
 /**
  * Define script
  */
 
 function delete_all() {
-  showHelp(cli)
-  console.log("Delete all")
+  showHelp(cli, [cli.input.length < 2])
+  
+  const destDir = path.resolve(cli.flags.destDir || '.')
 
+  console.log(`\nAre you sure you want to delete? ${chalk.red.bold(cli.input[1])} from ${chalk.green.bold(path.resolve(destDir))}`)
+  // TODO prompt user for confirmation
+  
   // Define the base case
   // Define the case where the element is a file
   // Define the case where the element is a subfolder
   // In the recursive call, change the arguments so that the function reaches the base case
-  
-  const pathsWithNodeModules = [
-    ['code', 'lambda', 'web-applications-I', 'lambda-calc'],
-    ['code', 'lambda', 'web-applications-I', 'gh-user-card'],
-    ['code', 'lambda', 'web-applications-II', 'NASA-APOD'],
-  ].map(path => path.join(...path)) // MS support path.join('code', 'lambda')
 
-  for (path of pathsWithNodeModules) {
-    childprocess.spawn('cd', [path])
-    childprocess.spanw('rm -rf', ['node_modules'])
+  
+  function buildPaths(root, match) {    
   }
+  
+  // const pathsWithNodeModules = [
+  //   ['code', 'lambda', 'web-applications-I', 'lambda-calc'],
+  //   ['code', 'lambda', 'web-applications-I', 'gh-user-card'],
+  //   ['code', 'lambda', 'web-applications-II', 'NASA-APOD'],
+  // ].map(path => path.join(...path)) // MS support path.join('code', 'lambda')
+
+  // for (path of pathsWithNodeModules) {
+  //   childprocess.spawn('cd', [path])
+  //   childprocess.spanw('rm -rf', ['node_modules'])
+  // }
 }
 
 /**
