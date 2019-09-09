dc9916ac4ea476b1fb2c984f9697c75199e990d3
Andy Bettisworth <andy@accreu.com>
2019-09-09 17:11:57 -0500

    Made use of dependency. Filtered results.
    Co-authored-by: Angel Torres <angeltorres.dev@gmail.com>

--- commands/weather.js
+++ commands/weather.js
@@ -6,6 +6,7 @@
 
 const meow = require('meow')
 const showHelp = require('../helpers/showHelp')
+const weather_js2 = require('weather-js2')
 
 /**
  * Parse args
@@ -22,7 +23,31 @@ const cli = meow(`
 
 function weather() {
   showHelp(cli)
-  console.log("weather")
+
+  const location = 'Houston, TX'
+
+  weather_js2.find({ search: location, degreeType: 'F', resCount: 2 }, (err, result) => {
+    if (err) console.error(err)
+
+    const res = result.reduce((acc, curr) => {
+      if (curr.location.name == location) {
+        return location
+      } else {
+        return acc
+      }
+    })
+
+    console.log(`${res.current.day} ${res.current.temperature} ℉  ${res.current.skytext}`)
+
+    // Filter out historic forecasts
+    res.forecast = res.forecast.filter(daily_forecast => daily_forecast.date >= res.current.date)
+
+    res.forecast.forEach(daily_forecast => {
+      console.log(daily_forecast.day)
+      console.log(daily_forecast.low),
+      console.log(daily_forecast.high)
+    })
+  })
 }
 
 /**
