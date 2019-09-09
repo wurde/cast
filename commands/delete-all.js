7f051faf06841c3d57bc00728d09c850b31bc2ec
Andy Bettisworth <andy@accreu.com>
2019-09-08 22:04:49 -0500

    Successfully recurse on directories. Added confirm prompt.
    Co-authored-by: Moses Samuel <mosamuel98@gmail.com>

--- commands/delete-all.js
+++ commands/delete-all.js
@@ -8,6 +8,7 @@ const meow = require('meow')
 const showHelp = require('../helpers/showHelp')
 const path = require('path')
 const chalk = require('chalk')
+const prompts = require('prompts')
 const fs = require('fs')
 
 /**
@@ -33,23 +34,45 @@ const cli = meow(`
  * Define script
  */
 
-function delete_all() {
+async function delete_all() {
   showHelp(cli, [cli.input.length < 2])
   
   const destDir = path.resolve(cli.flags.destDir || '.')
 
-  console.log(`\nAre you sure you want to delete? ${chalk.red.bold(cli.input[1])} from ${chalk.green.bold(path.resolve(destDir))}`)
-  // TODO prompt user for confirmation
-  
+  console.log('')
+  const confirmPrompt = await prompts({
+    type: 'confirm',
+    name: 'value',
+    message: `Are you sure you want to delete? ${chalk.red.bold(cli.input[1])} from ${chalk.green.bold(path.resolve(destDir))} [Y/n]`,
+    initial: true
+  })
+  if (!confirmPrompt.value) process.exit(1)
+
   // Define the base case
   // Define the case where the element is a file
   // Define the case where the element is a subfolder
   // In the recursive call, change the arguments so that the function reaches the base case
 
+  buildPaths(destDir)
   
-  function buildPaths(root, match) {    
+  function buildPaths(fromDir, match) {  
+    const files = fs.readdirSync(fromDir, { withFileTypes: true })
+    const matches = []
+
+    // match all cases
+    return files.reduce((acc, cur) => {
+      // case where cur is a file
+      // if (cur.isFile() && cur.name === match) {
+        // return acc.concat([path.resolve(cur.name)])
+        
+      // case where cur is a subfolder
+      if (cur.isDirectory()) { 
+        console.log(path.resolve(fromDir, cur.name))
+        return buildPaths(path.resolve(fromDir, cur.name), match)
+      }
+    }, [])
   }
-  
+
   // const pathsWithNodeModules = [
   //   ['code', 'lambda', 'web-applications-I', 'lambda-calc'],
   //   ['code', 'lambda', 'web-applications-I', 'gh-user-card'],
