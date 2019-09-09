57acb69d861549a64263ba50351bea8afa851599
Andy Bettisworth <andy@accreu.com>
2019-09-08 20:31:00 -0500

    Define pseudo-code
    Co-authored-by: Moses Samuel <mosamuel98@gmail.com>

--- commands/delete-all.js
+++ commands/delete-all.js
@@ -23,6 +23,22 @@ const cli = meow(`
 function delete_all() {
   showHelp(cli)
   console.log("Delete all")
+
+  // Define the base case
+  // Define the case where the element is a file
+  // Define the case where the element is a subfolder
+  // In the recursive call, change the arguments so that the function reaches the base case
+  
+  const pathsWithNodeModules = [
+    ['code', 'lambda', 'web-applications-I', 'lambda-calc'],
+    ['code', 'lambda', 'web-applications-I', 'gh-user-card'],
+    ['code', 'lambda', 'web-applications-II', 'NASA-APOD'],
+  ].map(path => path.join(...path)) // MS support path.join('code', 'lambda')
+
+  for (path of pathsWithNodeModules) {
+    childprocess.spawn('cd', [path])
+    childprocess.spanw('rm -rf', ['node_modules'])
+  }
 }
 
 /**
