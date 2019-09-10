80c93db0e1ce1de29e3504616943eea4c0375829
Andy Bettisworth <andy@accreu.com>
2019-09-09 20:00:52 -0500

    Filtered weather codes for icon display. Added a table to display daily forecasts.
    Co-authored-by: Angel Torres <angeltorres.dev@gmail.com>

--- commands/weather.js
+++ commands/weather.js
@@ -7,6 +7,9 @@
 const meow = require('meow')
 const showHelp = require('../helpers/showHelp')
 const weather_js2 = require('weather-js2')
+const {table} = require('table')
+const chalk = require('chalk')
+const figlet = require('figlet')
 
 /**
  * Parse args
@@ -18,6 +21,19 @@ const cli = meow(`
 `)
 
 /**
+ * Define helpers
+ */
+
+function figlet_text(text) {
+  return new Promise((resolve, reject) => {
+    figlet.text(text, (err, data) => {
+      if (err) reject(err)
+      resolve(data)
+    })
+  })
+}
+
+ /**
  * Define script
  */
 
@@ -26,8 +42,11 @@ function weather() {
 
   const location = 'Houston, TX'
 
-  weather_js2.find({ search: location, degreeType: 'F', resCount: 2 }, (err, result) => {
-    if (err) console.error(err)
+  weather_js2.find({ search: location, degreeType: 'F', resCount: 2 }, async (err, result) => {
+    if (err) {
+      console.error(err)
+      process.exit(1)
+    }
 
     const res = result.reduce((acc, curr) => {
       if (curr.location.name == location) {
@@ -37,16 +56,35 @@ function weather() {
       }
     })
 
-    console.log(`${res.current.day} ${res.current.temperature} ℉  ${res.current.skytext}`)
+    console.log(`\nLocation: ${res.location.name}\n`)
+    console.log(`${chalk.yellow.bold('\u2600')}  ${res.current.skytext}`)
+    console.log(`   Today ${res.current.temperature} ℉`)
+    console.log(`   ${res.current.winddisplay}`)
+    console.log('')
 
     // Filter out historic forecasts
     res.forecast = res.forecast.filter(daily_forecast => daily_forecast.date >= res.current.date)
 
-    res.forecast.forEach(daily_forecast => {
-      console.log(daily_forecast.day)
-      console.log(daily_forecast.low),
-      console.log(daily_forecast.high)
+    const res_array = res.forecast.map(daily_forecast => {
+      let icon
+      if (['0', '1', '2', '3', '4', '11', '12'].includes(daily_forecast.skycodeday)) {
+        icon = chalk.blue.bold('\u2602') // thunderstorm
+      } else if (['5', '6', '7', '8', '9', '10', '13', '14', '15', '16'].includes(daily_forecast.skycodeday)) {
+        icon = '' // rain sleet snow
+      } else if (['31', '32', '33', '34'].includes(daily_forecast.skycodeday)) {
+        icon = chalk.yellow.bold('\u2600') // sunny
+      } else if (['26', '27', '28', '29', '30'].includes(daily_forecast.skycodeday)) {
+        icon = chalk.white.bold('\u2601') // cloudy
+      } else {
+        console.log(daily_forecast.skycodeday)
+        icon = ' '
+      }
+
+      return `${icon}  ${daily_forecast.skytextday}\n   ${daily_forecast.shortday} ${chalk.green.bold(daily_forecast.high)} - ${chalk.green.bold(daily_forecast.low)} ℉\n   Precipitation ${daily_forecast.precip}%`
     })
+
+    console.log(table([res_array]))
+    console.log('')
   })
 }
 
