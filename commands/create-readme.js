afaf42330f0f421ca7584c26d51087e23b78c603
Andy Bettisworth <andy@accreu.com>
2019-09-16 21:58:00 -0500

    Ask all questions

--- commands/create-readme.js
+++ commands/create-readme.js
@@ -36,7 +36,48 @@ async function create_readme() {
       format: value => {
         return strip_ansi(value)
       }
-    }
+    },
+    {
+      type: 'text',
+      name: 'projectDescription',
+      message: 'Project description:'
+    },
+    {
+      type: 'confirm',
+      name: 'hasGettingStarted',
+      message: 'Add "Getting started" section?',
+      initial: true,
+      onRender() {
+        this.yesOption = chalk.blue.bold(this.yesOption)
+      }
+    },
+    {
+      type: 'confirm',
+      name: 'hasExamples',
+      message: 'Add "Examples" section?',
+      initial: true,
+      onRender() {
+        this.yesOption = chalk.blue.bold(this.yesOption)
+      }
+    },
+    {
+      type: 'text',
+      name: 'licenseName',
+      message: 'License name:',
+      initial: chalk.blue.bold('MIT'),
+      format: value => {
+        return strip_ansi(value)
+      }
+    },
+    {
+      type: 'text',
+      name: 'licenseUrl',
+      message: 'License url:',
+      initial: chalk.blue.bold('LICENSE.md'),
+      format: value => {
+        return strip_ansi(value)
+      }
+    },
   ])
 
   console.log('response', response)
