bdba039b8d1b940b724993207cb85d138db8ec88
Andy Bettisworth <andy@accreu.com>
2019-10-06 21:09:27 -0500

    Add description

--- commands/schedule.js
+++ commands/schedule.js
@@ -14,15 +14,18 @@ const showHelp = require('../helpers/showHelp')
 
 const cli = meow(`
   Usage
-    $ cast schedule
+    $ cast schedule [options] FILE
 
   Cron Examples
     * * * * * *           Every second.
     * 10 * * * *          Every ten minutes.
     0 */10 * * * *        Every tenth minute.
+    00 00 00 * * *        Every midnight.
     0 */30 9-17 * * *     Every 30 minutes between 9-17.
     * 4-22 * * 1-5        Every Minute Between hours 4-22, Monday through Friday.
-`)
+`, {
+  description: 'Execute something on a schedule.'
+})
 
 /**
  * Define script
