fcae0817d6a2ee9c4d5b40d755f86976b71140bf
Andy Bettisworth <andy@accreu.com>
2019-10-09 09:21:59 -0500

    Add sanity checks

--- helpers/didYouMean.js
+++ helpers/didYouMean.js
@@ -1,6 +1,25 @@
 'use strict'
 
 /**
+ * Define Levenshtein class
+ */
+
+class Levenshtein {
+  constructor(a, b) {
+    this.a = a
+    this.b = b
+  }
+
+  distance() {
+    if (this.a === this.b) return 0;
+    if (this.a === '') return this.b.length;
+    if (this.b === '') return this.a.length;
+
+    return 1
+  }
+}
+
+ /**
  * Define helper
  */
 
@@ -8,6 +27,17 @@ function didYouMean(list, search) {
   const matches = []
 
   if (list.constructor == String) list = list.split(' ')
+
+  list.map(item => {
+    const l = new Levenshtein(item, search)
+    console.log(`${item} ${search} ${l.distance()}`)
+
+    if (l.distance() <= 3) {
+      return item
+    } else {
+      return null
+    }
+  })
 
   return matches
 }
