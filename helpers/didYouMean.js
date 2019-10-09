a54ec8cd0cc4ae6921594b0a2871c8824de9cb1c
Andy Bettisworth <andy@accreu.com>
2019-10-09 09:42:54 -0500

    Calculate Levenshtein distance

--- helpers/didYouMean.js
+++ helpers/didYouMean.js
@@ -10,12 +10,48 @@ class Levenshtein {
     this.b = b
   }
 
+  forEach(array, fn) {
+    let i = -1
+
+    while (++i < array.length) {
+      fn(array[i], i, array)
+    }
+  }
+
   distance() {
     if (this.a === this.b) return 0;
     if (this.a === '') return this.b.length;
     if (this.b === '') return this.a.length;
 
-    return 1
+    let current
+    let matrix = []
+    let previous = [0]
+
+    this.forEach(this.a, (_, i) => { i++ , previous[i] = i })
+
+    matrix[0] = previous
+
+    this.forEach(this.b, (_, b_index) => {
+      current = [++b_index]
+
+      this.forEach(this.a, (_, a_index) => {
+        a_index++
+
+        if (this.a.charAt(a_index - 1) == this.b.charAt(b_index - 1))
+          current[a_index] = previous[a_index - 1]
+        else
+          current[a_index] = Math.min(
+            previous[a_index] + 1,
+            current[a_index - 1] + 1,
+            previous[a_index - 1] + 1
+          )
+      })
+
+      previous = current
+      matrix[matrix.length] = previous
+    })
+
+    return current[current.length - 1]
   }
 }
 
@@ -24,19 +60,11 @@ class Levenshtein {
  */
 
 function didYouMean(list, search) {
-  const matches = []
-
   if (list.constructor == String) list = list.split(' ')
 
-  list.map(item => {
+  const matches = list.filter(item => {
     const l = new Levenshtein(item, search)
-    console.log(`${item} ${search} ${l.distance()}`)
-
-    if (l.distance() <= 3) {
-      return item
-    } else {
-      return null
-    }
+    if (l.distance() <= 3) return item
   })
 
   return matches
