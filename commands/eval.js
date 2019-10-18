c0df4cee3c07c379c13c68598da62779cd515fd4
Andy Bettisworth <andy@accreu.com>
2019-10-18 09:53:06 -0500

    Init eval command

---
+++ commands/eval.js
@@ -0,0 +1,34 @@
+'use strict'
+
+/**
+ * Dependencies
+ */
+
+const meow = require('meow')
+const showHelp = require('../helpers/showHelp')
+
+/**
+ * Parse args
+ */
+
+const cli = meow(`
+  Usage
+    $ cast eval EXPRESSION
+`, {
+  description: 'Evaluate an expression, statement, or sequence of statements.'
+})
+
+/**
+ * Define script
+ */
+
+function run_eval() {
+  showHelp(cli)
+  console.log('eval')
+}
+
+/**
+ * Export script
+ */
+
+module.exports = run_eval
