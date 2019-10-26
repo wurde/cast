be713b7e3800fcf33c6f1cc31ef9591d66f26825
Andy Bettisworth <andy@accreu.com>
2019-10-25 20:15:32 -0500

    Test 'Decimal to binary conversion'

--- test/binary.test.js
+++ test/binary.test.js
@@ -9,6 +9,13 @@ const binary = require('../commands/binary')
  */
 
 describe('binary.js', () => {
+  test('Decimal to binary conversion', () => {
+    const x = binary(['4', '64', '256'])
+    expect(x[4]).toBe('100')
+    expect(x[64]).toBe('1000000')
+    expect(x[256]).toBe('100000000')
+  })
+
   test('Binary to decimal conversion', () => {
     const x = binary(['1010', '1111'], { reverse: true })
     expect(x['1010']).toBe(10)
