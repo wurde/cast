cddad3fde3fa71ea8fd37149f9f699ef7c789fc0
Andy Bettisworth <andy@accreu.com>
2019-10-27 04:03:41 -0500

    Install axios && initial dl command

--- commands/dl.js
+++ commands/dl.js
@@ -4,9 +4,15 @@
  * Dependencies
  */
 
-const https = require('https')
+const fs = require('fs')
+const url = require('url')
+const path = require('path')
+const axios = require('axios')
+const chalk = require('chalk')
 const meow = require('meow')
+const prompts = require('prompts')
 const showHelp = require('../helpers/showHelp')
+const printError = require('../helpers/printError')
 
 /**
  * Parse args
@@ -23,12 +29,51 @@ const cli = meow(`
  * Define script
  */
 
-function dl(url=null) {
-  showHelp(cli, [(!url && cli.input.length < 2)])
+async function dl(targetUrl=null) {
+  showHelp(cli)
 
-  const targetUrl = url || cli.input[1]
+  targetUrl = targetUrl || cli.input[1]
 
-  console.log('dl', targetUrl)
+  if (!targetUrl) {
+    console.log('')
+    const userResponse = await prompts({
+      type: 'text',
+      name: 'value',
+      message: chalk.blue.bold('Target URL:'),
+      validate: value => {
+        const parsedUrl = url.parse(value)
+        return (!parsedUrl.hostname) ? 'Error: Invalid URL' : true
+      }
+    })
+    console.log('')
+    targetUrl = userResponse.value
+  }
+
+  try {
+    // Validate the URL.
+    const parsedUrl = url.parse(targetUrl)
+    if (!parsedUrl.hostname) printError('Error: Invalid URL', cli)
+    targetUrl = parsedUrl.href
+
+    // Build output file path.
+    let filePath
+    if (parsedUrl.pathname && parsedUrl.pathname.length > 1) {
+      filePath = path.basename(parsedUrl.pathname)
+    } else {
+      filePath = Date.now()
+    }
+
+    // Fetch resource.
+    axios.get(targetUrl, { responseType: 'stream' }).then((res) => {
+      res.data.pipe(fs.createWriteStream(filePath))
+
+      if (arguments.length === 0) {
+        console.log(chalk.green.bold(`Successfully downloaded: ${filePath}`))
+      }
+    })
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 /**
