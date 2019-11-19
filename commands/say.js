46c00db2f431ddf560aaff6d3f68d9cd8372c105
Andy Bettisworth <andy@accreu.com>
2019-11-19 01:34:58 -0600

    Send message to spd-say

--- commands/say.js
+++ commands/say.js
@@ -4,8 +4,16 @@
  * Dependencies
  */
 
+const child_process = require('child_process');
 const meow = require('meow');
 const showHelp = require('../helpers/showHelp');
+const which = require('../helpers/which');
+
+/**
+ * Constants
+ */
+
+const hasSpdSay = which('spd-say');
 
 /**
  * Parse args
@@ -22,9 +30,16 @@ const cli = meow(`
  * Define script
  */
 
-function say() {
-  showHelp(cli);
-  console.log('say');
+function say(msg=null) {
+  showHelp(cli, [(!msg && cli.input.length < 2)]);
+
+  msg = msg || cli.input.slice(1).join(' ');
+
+  if (!hasSpdSay) {
+    throw new Error('Missing dependency: `spd-say`');
+  }
+
+  child_process.execSync(`spd-say '${msg}'`, { encoding: 'utf8' });
 }
 
 /**
