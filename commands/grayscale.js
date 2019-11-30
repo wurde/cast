e0fe79fe6c1b5f84236b5031d24647fda30f7185
Andy Bettisworth <andy@accreu.com>
2019-11-30 14:41:17 -0600

    Add createFilename helper

--- commands/grayscale.js
+++ commands/grayscale.js
@@ -5,9 +5,22 @@
  */
 
 const fs = require('fs');
+const path = require('path');
 const meow = require('meow');
 const jimp = require('jimp');
 const showHelp = require('../helpers/showHelp');
+
+/**
+ * Define helpers
+ */
+
+function createFilename(image) {
+  const ext = path.extname(image);
+  const basename = path.basename(image, ext);
+  const dirname = path.dirname(image);
+
+  return path.join(dirname, `${basename}-bw${ext}`)
+}
 
 /**
  * Parse args
@@ -28,11 +41,13 @@ async function grayscale(image) {
   showHelp(cli, [(!image && cli.input.length < 2)]);
 
   image = image || cli.input[1];
+  createFilename(image);
+  process.exit(0);
 
   try {
     if (fs.existsSync(image)) {
       const img = await jimp.read(image);
-
+      const out = createFilename(image);
       return img.greyscale().write('image-greyscale.jpg') ;
     } else {
       throw new Error(`MissingFile: no such image ${image}`);
