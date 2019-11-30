1875094aebb51edc606cc18f2c6e2cd41b108c1a
Andy Bettisworth <andy@accreu.com>
2019-11-30 14:09:03 -0600

    Desaturate image to grayscale

--- commands/grayscale.js
+++ commands/grayscale.js
@@ -4,6 +4,7 @@
  * Dependencies
  */
 
+const fs = require('fs');
 const meow = require('meow');
 const jimp = require('jimp');
 const showHelp = require('../helpers/showHelp');
@@ -14,7 +15,7 @@ const showHelp = require('../helpers/showHelp');
 
 const cli = meow(`
   Usage
-    $ cast grayscale
+    $ cast grayscale IMAGE
 `, {
   description: 'Change grayscale values of an image.'
 });
@@ -23,11 +24,20 @@ const cli = meow(`
  * Define script
  */
 
-function grayscale() {
-  showHelp(cli)
+function grayscale(image) {
+  showHelp(cli, [(!image && cli.input.length < 2)]);
 
+  image = image || cli.input[1];
 
-  console.log('grayscale');
+  if (fs.existsSync(image)) {
+    jimp.read(image)
+      .then(file => {
+        return file.greyscale().write('image-greyscale.jpg') 
+      })
+      .catch(err => console.error(err));
+  } else {
+    throw new Error(`MissingFile: no such image ${image}`);
+  }
 }
 
 /**
