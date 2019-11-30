ab4a575d3a4feda690b4ea1ae5913ec6c073425a
Andy Bettisworth <andy@accreu.com>
2019-11-30 13:55:42 -0600

    add grayscale command

---
+++ commands/grayscale.js
@@ -0,0 +1,36 @@
+'use strict'
+
+/**
+ * Dependencies
+ */
+
+const meow = require('meow');
+const showHelp = require('../helpers/showHelp');
+
+/**
+ * Parse args
+ */
+
+const cli = meow(`
+  Usage
+    $ cast grayscale
+`, {
+  description: 'Change grayscale values of an image.'
+});
+
+/**
+ * Define script
+ */
+
+function grayscale() {
+  showHelp(cli)
+
+
+  console.log('grayscale');
+}
+
+/**
+ * Export script
+ */
+
+module.exports = grayscale;
