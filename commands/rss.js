569eae62ac5f9061b251d4e6641e34e688cb3227
Andy Bettisworth <andy@accreu.com>
2019-12-05 13:05:03 -0600

    Add TODO

--- commands/rss.js
+++ commands/rss.js
@@ -25,6 +25,15 @@ const cli = meow(`
 function rss() {
   showHelp(cli)
 
+  // TODO default - fetch and print most recent articles.
+  // TODO fetch articles from a specific feed.
+  // TODO allow filtering articles by keyword.
+  // TODO print all feeds (show subscriptions top).
+  // TODO add a feed.
+  // TODO remove a feed.
+  // TODO subscribe to feed.
+  // TODO unsubscribe from feed.
+
   console.log('rss')
 };
 
