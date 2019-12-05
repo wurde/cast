0235a0a92b4d69ae6784f9d05524947f3c61ffee
Andy Bettisworth <andy@accreu.com>
2019-12-05 14:09:34 -0600

    Setup the database tables

--- commands/rss.js
+++ commands/rss.js
@@ -4,11 +4,13 @@
  * Dependencies
  */
 
+const path = require('path');
 const meow = require('meow');
 const Parser = require('rss-parser');
 const Sequelize = require('sequelize');
-const showHelp = require('../helpers/showHelp');
 const rssFeeds = require('../data/rss_feeds.json');
+const showHelp = require('../helpers/showHelp');
+const Database = require('../helpers/Database');
 
 /**
  * Constants
@@ -19,14 +21,16 @@ const parser = new Parser();
 // To get around this, you can use a proxy.
 const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
 const DB_PATH = path.join(process.env.HOME, '.rss.sqlite3');
-const queries = {
-  createTables: () => `
+const QUERIES = {
+  createTableFeeds: () => `
     CREATE TABLE IF NOT EXISTS feeds (
       id integer PRIMARY KEY,
       title text,
       link text,
       created_at timestamp DEFAULT CURRENT_TIMESTAMP
     );
+  `,
+  createTableArticles: () => `
     CREATE TABLE IF NOT EXISTS articles (
       id integer PRIMARY KEY,
       article_id integer ,
@@ -38,7 +42,26 @@ const queries = {
         ON DELETE CASCADE
     );
   `,
+  hasTable: tblName => `
+    SELECT name
+    FROM sqlite_master
+    WHERE type='table'
+    AND name='${tblName}';
+  `
 };
+
+/**
+ * Define helpers
+ */
+
+async function createTablesIfMissing(db) {
+  const [tblFeedsSelect] = await db.exec('hasTable', ['feeds']);
+  if (tblFeedsSelect.length === 0) await db.exec('createTableFeeds');
+
+  const [tblArticlesSelect] = await db.exec('hasTable', ['articles']);
+  if (tblArticlesSelect.length === 0)
+    await db.exec('createTableArticles');
+}
 
 /**
  * Parse args
@@ -55,19 +78,32 @@ const cli = meow(`
  * Define script
  */
 
-function rss() {
-  showHelp(cli)
+async function rss() {
+  showHelp(cli);
 
-  // TODO default - fetch and print most recent articles.
-  // TODO fetch articles from a specific feed.
-  // TODO allow filtering articles by keyword.
-  // TODO print all feeds (show subscriptions top).
-  // TODO add a feed.
-  // TODO remove a feed.
-  // TODO subscribe to feed.
-  // TODO unsubscribe from feed.
+  const db = new Database(DB_PATH, QUERIES);
+  await createTablesIfMissing(db);
 
-  console.log('rss', rssFeeds);
+  try {
+    console.log('DB_PATH', DB_PATH)
+    console.log('QUERIES', QUERIES)
+    console.log('db', db)
+
+    // TODO default - fetch and print most recent articles.
+    // TODO fetch articles from a specific feed.
+    // TODO allow filtering articles by keyword.
+    // TODO print all feeds (show subscriptions top).
+    // TODO add a feed.
+    // TODO remove a feed.
+    // TODO subscribe to feed.
+    // TODO unsubscribe from feed.
+
+    console.log('rss', rssFeeds);
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await db.close();
+  };
 };
 
 /**
