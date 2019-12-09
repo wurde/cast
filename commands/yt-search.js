5fb7f94774d7c6136f058585654ede36038b8ee5
Andy Bettisworth <andy@accreu.com>
2019-12-09 16:11:41 -0600

    Add launchBrowser helper

--- commands/yt-search.js
+++ commands/yt-search.js
@@ -6,6 +6,7 @@
 
 const meow = require('meow');
 const showHelp = require('../helpers/showHelp');
+const launchBrowser = require('../helpers/launchBrowser');
 
 /**
  * Parse args
@@ -22,12 +23,32 @@ const cli = meow(`
  * Define script
  */
 
-function yt_search(query = null) {
+async function yt_search(query = null) {
   showHelp(cli, [(!query && cli.input.length < 2)]);
 
   query = query || cli.input.slice(1).join(' ');
 
-  console.log('query', query);
+  const browser = await launchBrowser({
+    headless: false,
+    delay: 400,
+    timeout: 0,
+    defaultViewport: {
+      width: 1024,
+      height: 800
+    }
+  });
+
+  let results = [];
+
+  try {
+    console.log('query', query);
+  } catch (err) {
+    console.error(err);
+    return err;
+  } finally {
+    browser.close();
+    return results;
+  };
 }
 
 /**
