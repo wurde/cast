f3216417bc77f37503df628710968ecce1d229d4
Andy Bettisworth <andy@accreu.com>
2019-12-11 14:59:06 -0600

    Add progress output

--- commands/yt-download.js
+++ commands/yt-download.js
@@ -6,6 +6,7 @@
 
 const fs = require('fs');
 const path = require('path');
+const readline = require('readline');
 const meow = require('meow');
 const chalk = require('chalk');
 const ytdl_core = require('ytdl-core');
@@ -73,9 +74,48 @@ async function yt_download(link_or_id = null, options = {}) {
   const dir = options.dir || cli.flags.dir;
   const outputPath = path.join(dir, output);
 
-  console.log(chalk.white.bold('\n  Downloading: ') + chalk.green.bold(link));
 
-  ytdl_core(link).pipe(fs.createWriteStream(outputPath));
+  try {
+    const video = ytdl_core(link);
+    let starttime;
+
+    video.on('error', err => { console.error(err) });
+
+    video.once('response', () => {
+      starttime = Date.now();
+      console.log(chalk.white.bold('\n  Downloading: ') + chalk.green.bold(link));
+    });
+
+    video.on('progress', (chunkLength, downloaded, total) => {
+      const percent = downloaded / total;
+      const downloadedMinutes = (Date.now() - starttime) / 1000 / 60;
+      readline.cursorTo(process.stdout, 0);
+      process.stdout.write(`  ${(percent * 100).toFixed(2)}% downloaded `);
+      process.stdout.write(
+        `(${(downloaded / 1024 / 1024).toFixed(2)}MB of ${(
+          total /
+          1024 /
+          1024
+        ).toFixed(2)}MB)\n`
+      );
+      process.stdout.write(`  running for: ${downloadedMinutes.toFixed(2)}minutes`);
+      process.stdout.write(
+        `, estimated time left: ${(
+          downloadedMinutes / percent -
+          downloadedMinutes
+        ).toFixed(2)}minutes `
+      );
+      readline.moveCursor(process.stdout, 0, -1);
+    });
+
+    video.on('end', () => {
+      process.stdout.write('\n\n');
+    });
+
+    video.pipe(fs.createWriteStream(outputPath));
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 /**
