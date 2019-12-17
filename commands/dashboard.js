60beb57446ba0e96c9b3c4a327aa3e245b078356
Andy Bettisworth <andy@accreu.com>
2019-12-17 17:13:31 -0600

    Render the date.
    Co-authored-by: Angel Torres <alas4224@gmail.com>

--- commands/dashboard.js
+++ commands/dashboard.js
@@ -7,6 +7,8 @@
 const meow = require('meow');
 const chalk = require('chalk');
 const scen = require('scen');
+const figlet = require('figlet');
+const moment = require('moment');
 const showHelp = require('../helpers/showHelp');
 
 /**
@@ -28,22 +30,33 @@ function dashboard() {
   showHelp(cli);
 
   const text = 'The quick brown fox \njumps over the lazy dog.';
+  const date = figlet.textSync(moment().format('MMMM Do YYYY, h:mm:ss a'), {
+    font: 'Small'
+  });
 
-  const output = scen(text, {
+  const output = scen(date, {
     title: 'Custom Title',
     padding: '2 10',
     textAlign: 'center',
     style: 'classic'
-  }).split('\n')
-  .map(line => line + '  ' + line)
-  .join('\n');
+  })
+
+  // const output = scen(date, {
+  //   title: 'Custom Title',
+  //   padding: '2 10',
+  //   textAlign: 'center',
+  //   style: 'classic'
+  // }).split('\n')
+  // .map(line => line + '  ' + line)
+  // .join('\n');
 
   console.log('');
   console.log(chalk.white.bold(output), '\n');
 
   // TODO render the date.
+
+  // TODO fit box to terminal width.
   // TODO render weather app.
-  // TODO solve for dynamic terminal width.
   // TODO render boxes side by side.
 }
 
