c31828378dea541220272b8e800db0419041bec2
Andy Bettisworth <andy@accreu.com>
2019-12-17 16:51:33 -0600

    Render box with label.
    Co-authored-by: Angel Torres <alas4224@gmail.com>

--- commands/dashboard.js
+++ commands/dashboard.js
@@ -5,6 +5,7 @@
  */
 
 const meow = require('meow');
+const scen = require('scen');
 const showHelp = require('../helpers/showHelp');
 
 /**
@@ -25,11 +26,19 @@ const cli = meow(`
 function dashboard() {
   showHelp(cli);
 
-  // TODO render box with label.
-  // TODO render multiple boxes.
-  // TODO render weather app inside.
+  const text = 'The quick brown fox \njumps over the lazy dog.';
 
-  console.log('dashboard');
+  console.log(
+    scen(text, {
+      title: 'Custom Title',
+      padding: '2 6',
+      textAlign: 'center',
+      style: 'classic'
+    })
+  );
+
+  // TODO render boxes side by side.
+  // TODO render weather app.
 }
 
 /**
