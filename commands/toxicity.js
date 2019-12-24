d039d08252560a0ac5daadbec210fb76d0a4837f
Andy Bettisworth <andy@accreu.com>
2019-12-24 12:46:00 -0600

    Add option for threshold

--- commands/toxicity.js
+++ commands/toxicity.js
@@ -5,8 +5,10 @@
  */
 
 const meow = require('meow');
-const showHelp = require('../helpers/showHelp');
+const tf = require('@tensorflow/tfjs-node');
 const tfjs_toxicity = require('@tensorflow-models/toxicity');
+const showHelp = require('../helpers/showHelp');
+const isMainCommand = require('../helpers/isMainCommand');
 
 /**
  * Constant
@@ -16,30 +18,59 @@ const tfjs_toxicity = require('@tensorflow-models/toxicity');
 const THRESHOLD = 0.9;
 
 /**
+ * Define helpers
+ */
+
+function prettyPrintResults(results) {
+  // TODO toxicity
+  // TODO identity_attack
+  // TODO insult
+  // TODO severe_toxicity
+  // TODO obscene
+  // TODO sexual_explicit
+  // TODO threat
+  console.log(results);
+}
+
+/**
  * Parse args
  */
 
 const cli = meow(`
   Usage
-    $ cast toxicity
+    $ cast toxicity INPUT
+
+  Options:
+    -t, --threshold N   Minimum confidence level 0..1 (Default 0.9)
 `, {
-  description: 'Detect whether text contains toxic content.'
+  description: 'Detect whether text contains toxic content.',
+  flags: {
+    threshold: {
+      type: 'string',
+      alias: 't'
+    }
+  }
 });
 
 /**
  * Define script
  */
 
-async function toxicity() {
-  showHelp(cli);
+async function toxicity(input, threshold = null) {
+  showHelp(cli, [!input && cli.input.length < 2]);
 
-  const model = await tfjs_toxicity.load(THRESHOLD);
+  input = input || cli.input.slice(1).join(' ');
+  threshold = Number(threshold || cli.flags.threshold ||  THRESHOLD);
 
-  const input = ['You are disgusting. You suck.'];
+  const model = await tfjs_toxicity.load(threshold);
 
-  const predictions = await model.classify(input);
+  const predictions = await model.classify([input]);
 
-  console.log('toxicity', predictions, predictions.constructor);
+  if (isMainCommand(module)) {
+    prettyPrintResults(predictions);
+  }
+
+  return predictions;
 }
 
 /**
