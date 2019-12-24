44054e7abd0f06a9570d1e205ed45ce150f3b7f8
Andy Bettisworth <andy@accreu.com>
2019-12-24 12:28:07 -0600

    Sample prediction

--- commands/toxicity.js
+++ commands/toxicity.js
@@ -6,6 +6,14 @@
 
 const meow = require('meow');
 const showHelp = require('../helpers/showHelp');
+const tfjs_toxicity = require('@tensorflow-models/toxicity');
+
+/**
+ * Constant
+ */
+
+// Minimum confidence level.
+const THRESHOLD = 0.9;
 
 /**
  * Parse args
@@ -22,10 +30,16 @@ const cli = meow(`
  * Define script
  */
 
-function toxicity() {
+async function toxicity() {
   showHelp(cli);
 
-  console.log('toxicity');
+  const model = await tfjs_toxicity.load(THRESHOLD);
+
+  const input = ['You are disgusting. You suck.'];
+
+  const predictions = await model.classify(input);
+
+  console.log('toxicity', predictions, predictions.constructor);
 }
 
 /**
