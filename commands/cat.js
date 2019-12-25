255ce75ea19d0cd67bb41c31c3e909c03c240434
Andy Bettisworth <andy@accreu.com>
2019-12-24 23:34:07 -0600

    style changes

--- commands/cat.js
+++ commands/cat.js
@@ -17,105 +17,9 @@ const showHelp = require('../helpers/showHelp');
 
 const THEME = {
   /**
-   * keyword in a regular Algol-style language
-   */
-  keyword: chalk.bold.hex('#c678dd'),
-
-  /**
-   * mathematical formula in text markup
-   */
-  formula: chalk.bold.hex('#c678dd'),
-
-  /**
-   * built-in or library object (constant, class, function)
-   */
-  built_in: chalk.bold.hex('#d19a66'),
-
-  /**
-   * user-defined type in a language with first-class syntactically significant types, like
-   * Haskell
-   */
-  type: chalk.bold.hex('#d19a66'),
-
-  /**
-   * special identifier for a built-in value ("true", "false", "null")
-   */
-  literal: chalk.bold.hex('#d19a66'),
-
-  /**
-   * number, including units and modifiers, if any.
-   */
-  number: chalk.bold.hex('#d19a66'),
-
-  /**
-   * literal regular expression
-   */
-  regexp: chalk.bold.hex('#98c379'),
-
-  /**
-   * literal string, character
-   */
-  string: chalk.bold.hex('#98c379'),
-
-  /**
-   * string within meta construct
-   */
-  'meta-string': chalk.bold.hex('#98c379'),
-
-  /**
-   * added or changed line in a diff
-   */
-  addition: chalk.bold.hex('#98c379'),
-
-  /**
-   * name of an attribute followed by a structured value part, like CSS properties
-   */
-  attribute: chalk.bold.hex('#98c379'),
-
-  /**
-   * symbolic constant, interned string, goto label
-   */
-  symbol: chalk.bold.hex('#61aeee'),
-
-  /**
-   * class or class-level declaration (interfaces, traits, modules, etc)
-   */
-  class: chalk.bold.hex('#e6c07b'),
-
-  /**
    * function or method declaration
    */
-  function: chalk.bold.hex('#c678dd'),
-
-  /**
-   * name of a class or a function at the place of declaration
-   */
-  title: chalk.bold.hex('#61aeee'),
-
-  /**
-   * block of function arguments (parameters) at the place of declaration
-   */
-  params: chalk.bold.hex('#c678dd'),
-
-  /**
-   * comment
-   */
-  comment: chalk.bold.hex('#5c6370'),
-
-  /**
-   * documentation markup within comments
-   */
-  doctag: chalk.bold.hex('#5c6370'),
-
-  /**
-   * flags, modifiers, annotations, processing instructions, preprocessor directive, etc
-   */
-  meta: chalk.bold.hex('#5c6370'),
-
-  /**
-   * quotation in text markup
-   */
-  quote: chalk.bold.hex('#5c6370'),
+  function: chalk.bold.yellow,
 
   /**
    * keyword or built-in within meta construct
@@ -128,30 +32,9 @@ const THEME = {
   tag: chalk.grey,
 
   /**
-   * name of an XML tag, the first word in an s-expression
-   */
-  name: chalk.bold.hex('#e6c07b'),
-
-  /**
    * s-expression name from the language standard library
    */
   'builtin-name': chalk.bold.hex('#e6c07b'),
-
-  /**
-   * name of an attribute with no language defined semantics (keys in JSON, setting names in
-   * .ini), also sub-attribute within another highlighted object, like XML tag
-   */
-  attr: chalk.cyan,
-
-  /**
-   * variable in a config or a template file, environment var expansion in a script
-   */
-  variable: chalk.bold.hex('#61aeee'),
-
-  /**
-   * list item bullet in text markup
-   */
-  bullet: chalk.bold.hex('#61aeee'),
 
   /**
    * code block in text markup
@@ -174,34 +57,29 @@ const THEME = {
   link: chalk.bold.hex('#61aeee').underline,
 
   /**
-   * #id selector in CSS
+   * comment
    */
-  'selector-id': chalk.bold.hex('#61aeee'),
+  comment: chalk.bold.hex('#5c6370').italic,
 
   /**
-   * .class selector in CSS
+   * quotation in text markup
    */
-  'selector-class': chalk.bold.hex('#61aeee'),
+  quote: chalk.bold.hex('#5c6370').italic,
 
   /**
-   * [attr] selector in CSS
+   * documentation markup within comments
    */
-  'selector-attr': chalk.bold.hex('#61aeee'),
+  doctag: chalk.bold.hex('#c678dd'),
 
   /**
-   * :pseudo selector in CSS
+   * keyword in a regular Algol-style language
    */
-  'selector-pseudo': chalk.bold.hex('#61aeee'),
+  keyword: chalk.bold.hex('#c678dd'),
 
   /**
-   * tag of a template language
+   * mathematical formula in text markup
    */
-  'template-tag': chalk.bold.hex('#e06c75'),
-
-  /**
-   * variable in a template language
-   */
-  'template-variable': chalk.bold.hex('#d19a66'),
+  formula: chalk.bold.hex('#c678dd'),
 
   /**
    * heading of a section in a config file, heading in text markup
@@ -209,14 +87,14 @@ const THEME = {
   section: chalk.bold.hex('#e06c75'),
 
   /**
+   * name of an XML tag, the first word in an s-expression
+   */
+  name: chalk.bold.hex('#e06c75'),
+
+  /**
    * tag selector in CSS
    */
   'selector-tag': chalk.bold.hex('#e06c75'),
-
-  /**
-   * parsed section inside a literal string
-   */
-  subst: chalk.bold.hex('#e06c75'),
 
   /**
    * deleted line in a diff
@@ -224,9 +102,131 @@ const THEME = {
   deletion: chalk.bold.hex('#e06c75'),
 
   /**
+   * tag of a template language
+   */
+  'template-tag': chalk.bold.hex('#e06c75'),
+
+  /**
+   * parsed section inside a literal string
+   */
+  subst: chalk.bold.hex('#e06c75'),
+
+  /**
+   * special identifier for a built-in value ("true", "false", "null")
+   */
+  literal: chalk.bold.hex('#56b6c2'),
+
+  /**
+   * literal string, character
+   */
+  string: chalk.bold.hex('#98c379'),
+
+  /**
+   * literal regular expression
+   */
+  regexp: chalk.bold.hex('#98c379'),
+
+  /**
+   * added or changed line in a diff
+   */
+  addition: chalk.bold.hex('#98c379'),
+
+  /**
+   * name of an attribute followed by a structured value part, like CSS properties
+   */
+  attribute: chalk.bold.hex('#98c379'),
+
+  /**
+   * string within meta construct
+   */
+  'meta-string': chalk.bold.hex('#98c379'),
+
+  /**
+   * built-in or library object (constant, class, function)
+   */
+  built_in: chalk.bold.hex('#e6c07b'),
+
+  /**
+   * class or class-level declaration (interfaces, traits, modules, etc)
+   */
+  class: chalk.bold.hex('#e6c07b'),
+
+  /**
+   * name of an attribute with no language defined semantics (keys in JSON, setting names in
+   * .ini), also sub-attribute within another highlighted object, like XML tag
+   */
+  attr: chalk.bold.hex('#d19a66'),
+
+  /**
+   * variable in a config or a template file, environment var expansion in a script
+   */
+  variable: chalk.bold.hex('#d19a66'),
+
+  /**
+   * variable in a template language
+   */
+  'template-variable': chalk.bold.hex('#d19a66'),
+
+  /**
+   * user-defined type in a language with first-class syntactically significant types, like
+   * Haskell
+   */
+  type: chalk.bold.hex('#d19a66'),
+
+  /**
+   * .class selector in CSS
+   */
+  'selector-class': chalk.bold.hex('#d19a66'),
+
+  /**
+   * [attr] selector in CSS
+   */
+  'selector-attr': chalk.bold.hex('#d19a66'),
+
+  /**
+   * :pseudo selector in CSS
+   */
+  'selector-pseudo': chalk.bold.hex('#d19a66'),
+
+  /**
+   * number, including units and modifiers, if any.
+   */
+  number: chalk.bold.hex('#d19a66'),
+
+  /**
+   * symbolic constant, interned string, goto label
+   */
+  symbol: chalk.bold.hex('#61aeee'),
+
+  /**
+   * list item bullet in text markup
+   */
+  bullet: chalk.bold.hex('#61aeee'),
+
+  /**
+   * flags, modifiers, annotations, processing instructions, preprocessor directive, etc
+   */
+  meta: chalk.bold.hex('#61aeee'),
+
+  /**
+   * #id selector in CSS
+   */
+  'selector-id': chalk.bold.hex('#61aeee'),
+
+  /**
+   * name of a class or a function at the place of declaration
+   */
+  title: chalk.bold.hex('#61aeee'),
+
+  /**
+   * block of function arguments (parameters) at the place of declaration
+   */
+  params: chalk.bold.hex('#abb2bf'),
+
+  /**
    * things not matched by any token
    */
-  default: chalk.bold.hex('#abb2bf')
+  // default: chalk.bold.hex('#abb2bf')
 };
 
 /**
