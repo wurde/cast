6d08f61166fb27a0714343f0d2f64926ece54c5d
Andy Bettisworth <andy@accreu.com>
2019-12-27 12:31:27 -0600

    Load data

--- helpers/Storage.js
+++ helpers/Storage.js
@@ -12,6 +12,17 @@ const path = require('path');
 class Storage {
   constructor(file) {
     this.path = path.resolve(file);
+    this.data = null;
+    this.load();
+  }
+
+  load() {
+    if (fs.existsSync(this.path)) {
+      this.data = fs.readFileSync(this.path, 'utf8');
+    } else {
+      fs.writeFileSync(this.path, '{}', 'utf8');
+      this.data = {};
+    }
   }
 }
 
