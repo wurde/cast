b91cd7217867164c9fe563cbeba5ac6325fc2465
Andy Bettisworth <andy@accreu.com>
2019-12-27 13:09:51 -0600

    Finish Proxy API

--- helpers/Storage.js
+++ helpers/Storage.js
@@ -14,15 +14,48 @@ class Storage {
     this.path = path.resolve(file);
     this.data = null;
     this.load();
+    return this.trap();
   }
 
   load() {
     if (fs.existsSync(this.path)) {
-      this.data = fs.readFileSync(this.path, 'utf8');
+      this.data = JSON.parse(fs.readFileSync(this.path, 'utf8'));
     } else {
       fs.writeFileSync(this.path, '{}', 'utf8');
       this.data = {};
     }
+  }
+
+  trap() {
+    return this.watch(this.data, () => {
+      process.nextTick(() => {
+        fs.writeFileSync(this.path, JSON.stringify(this.data, null, 2), 'utf8');
+      });
+    });
+  }
+
+  watch(target, callback) {
+    const handler = {};
+
+    handler.get = (target, property, receiver) => {
+      try {
+        return new Proxy(target[property], handler);
+      } catch (error) {
+        return Reflect.get(target, property, receiver);
+      }
+    };
+
+    handler.set = (target, property, value) => {
+      callback();
+      return Reflect.set(target, property, value);
+    };
+
+    handler.deleteProperty = (target, property) => {
+      callback();
+      return Reflect.deleteProperty(target, property);
+    };
+
+    return new Proxy(target, handler);
   }
 }
 
