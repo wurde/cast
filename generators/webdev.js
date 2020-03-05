19b08033a5fcf0525fcaff2d095867cd0116905f
Andy Bettisworth <andy@accreu.com>
2020-03-05 00:43:27 -0600

    Spec generator

--- generators/webdev.js
+++ generators/webdev.js
@@ -1,7 +1,29 @@
 'use strict'
 
+const chalk = require('chalk');
+
 function main() {
-  console.log('Generating webdev environment...');
+  console.log(chalk.white.bold(`
+    /////////////////
+    Generator:`, chalk.green.bold('webdev\n')
+  ));
+
+  // Check if .git/ already exists.
+
+  // Copy files:
+  //   LICENSE
+  //   .editorconfig
+  //   tsconfig.json
+  //   Dockerfile
+  //   server.js
+  //   webpack.config.js
+  //   package.json
+  //   public/index.html
+  //   src/index.ts
+
+  // Npm install.
+
+  // Git init commit.
 }
 
 module.exports = main();
