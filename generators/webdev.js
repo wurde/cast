d42d36264cbaf93481b9a09ca5ab8fff900bef1e
Andy Bettisworth <andy@accreu.com>
2020-03-05 01:03:35 -0600

    Write template files

--- generators/webdev.js
+++ generators/webdev.js
@@ -3,6 +3,15 @@
 const fs = require('fs');
 const path = require('path');
 const chalk = require('chalk');
+const mkdir = require("../helpers/mkdir");
+
+const templates = [
+  "LICENSE",
+  ".editorconfig",
+  "server.js",
+  "tsconfig.json",
+  "webpack.config.js"
+];
 
 function main() {
   console.log(chalk.white.bold(`
@@ -16,16 +25,74 @@ function main() {
     process.exit(1);
   }
 
-  // Copy files:
-  //   LICENSE
-  //   .editorconfig
-  //   tsconfig.json
-  //   Dockerfile
-  //   server.js
-  //   webpack.config.js
+  // Copying templates.
+  for (const template of templates) {
+    fs.copyFileSync(path.join(__dirname, "../templates", template), template);
+  }
+
+  // Write files:
   //   package.json
-  //   public/index.html
-  //   src/index.ts
+  //   Dockerfile
+
+  // Write public index.html file.
+  mkdir("public");
+  fs.writeFileSync("public/index.html", `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>My App</title>
+  </head>
+  <body>
+    <my-app></my-app>
+    <script src="./index.js" type="module"></script>
+  </body>
+</html>
+  `);
+
+  // Write source index.ts file.
+  mkdir("src");
+  fs.writeFileSync("src/index.ts", `
+import { html, css, LitElement } from 'lit-element';
+
+export class FooBar extends LitElement {
+  static get styles() {
+    return css\`
+      :host {
+        --foo-bar-text-color: #000;
+
+        display: block;
+        padding: 25px;
+        color: var(--foo-bar-text-color);
+      }
+    \`;
+  }
+
+  static get properties() {
+    return {
+      title: { type: String },
+      counter: { type: Number },
+    };
+  }
+
+  constructor() {
+    super();
+    this.title = 'Hey there';
+    this.counter = 5;
+  }
+
+  __increment() {
+    this.counter += 1;
+  }
+
+  render() {
+    return html\`
+      <h2>$\{this.title} Nr. $\{this.counter}!</h2>
+      <button @click=$\{this.__increment}>increment</button>
+    \`;
+  }
+}
+  `);
 
   // Npm install.
 
---
+++ templates/webpack.config.js
@@ -0,0 +1,36 @@
+const TerserPlugin = require("terser-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          output: {
+            comments: false
+          }
+        },
+        extractComments: false
+      })
+    ]
+  }
+};
