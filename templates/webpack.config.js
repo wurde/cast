b2bbe961458f087852857771eeee6463499dd93e
Andy Bettisworth <andy.bettisworth@gmail.com>
2020-12-06 12:50:14 -0600

    rm webpack config

--- templates/webpack.config.js
+++ templates/webpack.config.js
@@ -21,9 +21,6 @@ module.exports = {
     filename: "index.js",
     path: path.resolve(__dirname, "dist")
   },
-  node: {
-    fs: "empty"
-  },
   plugins: [
     new Dotenv({
       path: "./.env"
