f7ceba6cc53f5dae6323abdf834a31a305085be2
Andy Bettisworth <andy.bettisworth@gmail.com>
2021-01-30 11:57:40 -0600

    support main branch

--- commands/gitsync.js
+++ commands/gitsync.js
@@ -14,7 +14,22 @@ const showHelp = require('../helpers/showHelp')
 
 const config = {
   cwd: process.cwd(),
-  stdio: [null, 'inherit', 'inherit']
+  encoding: 'utf8',
+  stdio: [null, 'pipe', 'inherit']
+}
+
+/**
+ * Helpers
+ */
+
+const master_or_main = (output) => {
+  const branches = output.split('\n').map(x => x.replace('* ', '').trim())
+
+  for (let i = 0; i < branches.length; i++) {
+    if (branches[i] === 'main') return 'main';
+    if (branches[i] === 'master') return 'master';
+  }
+  return 'master'
 }
 
 /**
@@ -34,14 +49,18 @@ const cli = meow(`
 
 function gitsync() {
   showHelp(cli)
+
+  const result = child_process.spawnSync('git', ['branch', '--list'], config)
+  const branch_name = master_or_main(result.output[1])
+
   child_process.spawnSync('git', ['add', '-A'], config)
   child_process.spawnSync('git', ['commit', '-m', 'gitsync cleanup commit'], config)
-  child_process.spawnSync('git', ['checkout', '--quiet', 'master'], config)
+  child_process.spawnSync('git', ['checkout', '--quiet', branch_name], config)
   child_process.spawnSync('git', ['merge', 'annex'], config)
   child_process.spawnSync('git', ['checkout', '--quiet', '-B', 'annex'], config)
-  child_process.spawnSync('git', ['checkout', '--quiet', 'master'], config)
-  child_process.spawnSync('git', ['pull', 'origin', 'master'], config)
-  child_process.spawnSync('git', ['push', 'origin', 'master'], config)
+  child_process.spawnSync('git', ['checkout', '--quiet', branch_name], config)
+  child_process.spawnSync('git', ['pull', 'origin', branch_name], config)
+  child_process.spawnSync('git', ['push', 'origin', branch_name], config)
   child_process.spawnSync('git', ['checkout', '--quiet', '-B', 'annex'], config)
 }
 
