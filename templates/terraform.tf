b1a52efcb1820b973594dc1c4313c4c7c73a853d
Andy Bettisworth <andy.bettisworth@gmail.com>
2021-10-17 01:50:09 -0500

    update template

--- templates/terraform.tf
+++ templates/terraform.tf
@@ -1,12 +1,13 @@
-# Configure Terraform.
-
+# https://www.terraform.io/docs/language/settings/index.html
+# Configure some behaviors of Terraform itself.
 terraform {
-  required_version = "~> 0.13"
+  # Specify which versions of Terraform can be used with this configuration.
+  required_version = "~> v1.1"
 
   # https://www.terraform.io/docs/backends/types/s3.html
   backend "s3" {
     bucket = "andys-terraform-backend"
-    region = "us-east-1"
+    region = "us-east-2"
     key    = "key/terraform.tfstate"
   }
 
@@ -15,4 +16,30 @@ terraform {
   #   bucket = "andys-terraform-backend"
   #   prefix = "key/terraform.tfstate"
   # }
+
+  required_providers {
+    # https://registry.terraform.io/providers/hashicorp/aws
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.61"
+    }
+
+    # https://registry.terraform.io/providers/hashicorp/google
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 3.86"
+    }
+
+    # https://registry.terraform.io/providers/hashicorp/local
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.1"
+    }
+
+    # https://registry.terraform.io/providers/hashicorp/random
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1"
+    }
+  }
 }
